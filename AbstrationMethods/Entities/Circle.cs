4073c15fb75108fd815f34ffd285c468d34d6243
simorai <109289730+simorai@users.noreply.github.com>
2024-12-30 19:31:50 +0000

    Building abstract Circle class

---
+++ AbstrationMethods/Entities/Circle.cs
@@ -0,0 +1,19 @@
+﻿using AbstrationMethods.Entities.Enums;
+
+namespace AbstrationMethods.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
